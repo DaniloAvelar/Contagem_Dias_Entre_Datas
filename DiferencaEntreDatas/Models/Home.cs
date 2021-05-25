ef98022d2d23cb556877fdfb23d3ee494c27a47c
DaniloAvelar <da.avelar@hotmail.com>
2021-05-25 10:23:15 -0300

    Criação do Modelo de Dados

---
+++ DiferencaEntreDatas/Models/Home.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiferencaEntreDatas.Models
+{
+    public class Home
+    {
+        //[Display(Name = "Data Inicial")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime Inicial { get; set; }
+
+        //[Display(Name = "Data Final")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime Final { get; set; }
+    }
+}
