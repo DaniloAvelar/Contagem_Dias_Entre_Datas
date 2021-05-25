a254129b17540266ec0abbf844f6caf4928f35c6
DaniloAvelar <da.avelar@hotmail.com>
2021-05-25 10:25:23 -0300

    Criação da Controller (Home) com as regras de calculo entre as datas

--- DiferencaEntreDatas/Controllers/HomeController.cs
+++ DiferencaEntreDatas/Controllers/HomeController.cs
@@ -20,6 +20,49 @@ namespace DiferencaEntreDatas.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Message = "Seja Bem Vindo(a)!";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(DateTime inicial, DateTime final)
+        {
+
+            if (inicial.ToString() != "" && final.ToString() != "")
+            {
+                //calcula anos
+                int Anos = new DateTime(final.Subtract(inicial).Ticks).Year - 1;
+                DateTime AnosTranscorridos = inicial.AddYears(Anos);
+
+                //calcula meses
+                int Meses = 0;
+
+                for (int i = 1; i <= 12; i++)
+                {
+                    if (AnosTranscorridos.AddMonths(i) == final)
+                    {
+                        Meses = i;
+                        break;
+                    }
+                    else if (AnosTranscorridos.AddMonths(i) >= final)
+                    {
+                        Meses = i - 1;
+                        break;
+                    }
+
+                }
+
+                //calcula dias
+                int Dias = final.Subtract(AnosTranscorridos.AddMonths(Meses)).Days;
+                //    //atribuindo os valores
+                var tdAno = Anos + "  ano(s)  " + Meses + "  mes(es) " + Dias + " dia(s)";
+
+                var Resultado = tdAno;
+
+                ViewBag.Message = "O Resultado entre ambas as datas são:";
+                ViewBag.result = Resultado;
+            }
+
             return View();
         }
 
